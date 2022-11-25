77f1d89d40ee27c99a89a5f2a46f336a0d1d6324
axad03213 <axad03213@gmail.com>
2022-11-25 03:57:55 -0800

    email already existing & same password

--- actions/register.php
+++ actions/register.php
@@ -10,12 +10,23 @@
     if($password == $cpassword){
         # connecting my software (jobish.com) with databse (jobish) ..
         $cn = mysqli_connect('localhost','root','','jobish') or die('Cant Connect With Database');
-        # Query ..
-        $query = "INSERT INTO `users`(name,email,password) VALUES ('$name','$email','$password')";
-        // var_dump($query); # this function is use for check query working ..
-        // mysqli_error($cn) it will display error if it will be in connection
-        mysqli_query($cn,$query) or die('Cant Run Query'.mysqli_error($cn));
+        $q = "SELECT * FROM `users` WHERE email='$email'";
+        $result = mysqli_query($cn,$q) or die("Query Can't run".mysqli_error($cn));
+        $row = mysqli_num_rows($result);
+        if($row >= 1){
+            $error = "Email already exist";
+            // header function will be redirect to register.php where form is working ..
+            header('location:../register.php?error='.$error);
+        } else {
+             // # Query ..
+            $query = "INSERT INTO `users`(name,email,password) VALUES ('$name','$email','$password')";
+            // var_dump($query); # this function is use for check query working ..
+            //mysqli_error($cn) it will display error if it will be in connection
+            mysqli_query($cn,$query) or die('Cant Run Query'.mysqli_error($cn));
+        }
     } else {
-        echo "Your Password Is Not Same";
+        $error = "Your Password Is Match, Try Again";
+        // header function will be redirect to register.php where form is working ..
+        header('location:../register.php?error='.$error);
     }
 ?>
--- register.php
+++ register.php
@@ -3,8 +3,17 @@
 		<div class="row">
 			<div class="col-lg-4">
 				<div class="card">
-					<div class="card-header"><i class="fa fa-user"></i>  Register Here </div>
+					<div class="card-header"><i class="fa fa-user"></i>  Register Here <a href="actions/test.phpname?=5">Click Here</a> </div>
 					<div class="card-body">
+						<?php 
+							if(!empty($_GET['error'])){
+						?>
+							<div class="alert alert-danger">
+								<b>Alert!</b> <?php echo $_GET['error']; ?>
+							</div>
+						<?php
+							}
+						?>
 						<form action="actions/register.php" method="post">
 							<div class="form-group">
 								<label> <i class="fa fa-user"></i> Name </label>
